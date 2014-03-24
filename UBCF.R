2375d8c314d7c1f89d61d73b0c718c842671b027
unknown <anandk@kanand.corporate.thoughtworks.com>
2014-03-24 17:35:22 +0530

    Removed formatting

--- UBCF.R
+++ UBCF.R
@@ -5,7 +5,6 @@ setwd("C:/TW-Projects/PS-Projects/AbcamAnalytics/RSandbox/recommender")
 df <- read.csv(file="FinalWeightsCombined",sep="\t",
                colClasses=c("character","character","numeric"))
 colnames(df)<- c("user","abIDs","weight")
-df$user <- format(df$user, scientific=FALSE)
 userItemMatrix <- as(df,"realRatingMatrix")
 rec <- Recommender(userItemMatrix, method="POPULAR")
 recom <- predict(rec,userItemMatrix[44],n=10)
