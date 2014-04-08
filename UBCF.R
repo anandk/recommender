9ec68bb318992f8ad3cc7a3ebed0a93cecfd6003
AK <anandk@thoughtworks.com>
2014-04-08 14:17:18 +0530

    Changes made

--- UBCF.R
+++ UBCF.R
@@ -6,9 +6,25 @@ df <- read.csv(file="FinalWeightsCombined",sep="\t",
                colClasses=c("character","character","numeric"))
 colnames(df)<- c("user","abIDs","weight")
 userItemMatrix <- as(df,"realRatingMatrix")
-rec <- Recommender(userItemMatrix, method="POPULAR")
+rec <- Recommender(userItemMatrix[1:1400], method="UBCF")
+finalMatr <- matrix(nrow=1,ncol=11)
+recom <- predict(rec,userItemMatrix[1401],n=3)
+for(index in 1:dim(userItemMatrix)[1])
+{
+  userID <- names(as(userItemMatrix[index],"list"))
+  recom <- predict(rec,userItemMatrix[userIndex],n=10)
+  finalMatr <- rbind(finalMatr, c(userID,unlist(as(recom,"list"))))
+  if(index%%100 == 0)
+    {
+      write.table(finalMatr[-1,], file="./Output.txt", sep=",", append=TRUE, row.names=FALSE,col.names=FALSE)
+      finalMatr <- matrix(nrow=1,ncol=11)
+    }  
+}
+write.table(finalMatr[-1,], file="./Output.txt", sep=",", append=TRUE, row.names=FALSE,col.names=FALSE)
+
+
+#userIndex <- 407983
 #as(recom,"list")
 userList <- as(userItemMatrix,"list")
 dfUser <- as(userList,"data.frame")
-userIndex <- which(colnames(dfUser)=="-6786791769037665337")
-recom <- predict(rec,userItemMatrix[407983],n=10)
+userIndex <- which(colnames(dfUser)=="8549280749670823302")
