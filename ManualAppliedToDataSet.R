1a8f677773170bf280aa5c2f11a98b823eab39b6
AK <anandk@thoughtworks.com>
2014-04-08 16:08:47 +0530

    Final unclean

--- ManualAppliedToDataSet.R
+++ ManualAppliedToDataSet.R
@@ -4,7 +4,10 @@ options("scipen"=100, "digits"=4)
 setwd("C:/TW-Projects/PS-Projects/AbcamAnalytics/RSandbox/recommender/")
 weightsDF <- read.csv(file="FinalWeightsCombined",sep="\t",colClasses=c("character","character","numeric"))
 colnames(weightsDF)<- c("user","abIDs","weight")
-viewedPages <- weightsDF[which(weightsDF[,1]=="-3685863687683693081"),2]
+targetUserID <- "-3685863687683693081"
+
+
+viewedPages <- weightsDF[which(weightsDF[,1]==targetUserID),2]
 usersWhoViewedTheSomeOfThosePages <- unique(weightsDF[weightsDF[,2] %in% viewedPages,1:2])
 pagesViewsBySimilarUsers <- aggregate(usersWhoViewedTheSomeOfThosePages[-1],usersWhoViewedTheSomeOfThosePages[1],c)
 pagesViewsBySimilarUsers$Count <- sapply(1:max(row(pagesViewsBySimilarUsers)),
@@ -13,7 +16,7 @@ pagesViewsBySimilarUsers$Count <- sapply(1:max(row(pagesViewsBySimilarUsers)),
 sortedDF <- pagesViewsBySimilarUsers[order(pagesViewsBySimilarUsers$Count,decreasing=T),]
 sortedDF <- sortedDF[-1,]
 mostRelevantUsers <- sortedDF[1:10,1]
-meAndRelevantUserViews <- weightsDF[weightsDF[,1] %in% c("-3685863687683693081",mostRelevantUsers),]
+meAndRelevantUserViews <- weightsDF[weightsDF[,1] %in% c(targetUserID,mostRelevantUsers),]
 rrm <- as(meAndRelevantUserViews,"realRatingMatrix")
 tmpdgcMatrix <- as(rrm,"dgCMatrix")
 tmpMatr <- cbind(row.names(tmpMatr),as(tmpdgcMatrix,"matrix"))
@@ -22,23 +25,35 @@ allUniqueAbIDs <- unique(weightsDF[,2])
 abIDsNotViewedByRelevantUsers <- setdiff(unique(weightsDF[,2]),colnames(tmpMatr))
 distMatr <- matrix(nrow=11,ncol=length(abIDsNotViewedByRelevantUsers),data=0)
 colnames(distMatr) <- abIDsNotViewedByRelevantUsers
-distMatr <- cbind(c(-3685863687683693081,as.numeric(mostRelevantUsers)),distMatr)
+distMatr <- cbind(c(targetUserID,mostRelevantUsers),distMatr)
 distMatr <- distMatr[order(distMatr[,1]),]
 distMatr <- cbind(distMatr,tmpMatr[,-1])
-distMatr <- rbind(distMatr[distMatr[,1] == "-3685863687683693081",], distMatr[distMatr[,1]!="-3685863687683693081",])
-distanceMatr <- dist(distMatr[,-1],method="Manhattan",diag=T,upper=T)
+distMatr <- rbind(distMatr[distMatr[,1] == targetUserID,], distMatr[distMatr[,1]!=targetUserID,])
+colnames(distMatr)[1] <- "userID"
+
+numericDistMatr <- matrix(as.numeric(distMatr[,-1]),nrow=11)
+colnames(numericDistMatr) <- colnames(distMatr)[-1]
+distanceMatr <- dist(numericDistMatr,method="Manhattan",diag=T,upper=T)
+
+userRelevanceWeights <- as.matrix(distanceMatr)[1,-1]
+highestWeight <- min(userRelevanceWeights)
+userRelevanceWeights <- highestWeight/userRelevanceWeights
+weightsMatrix <- matrix(userRelevanceWeights,nrow=10)
+weightedRecom <- numericDistMatr[-1,] * weightsMatrix[,1]
+weightedRecom <- rbind(weightedRecom,colSums(weightedRecom))
+nonZeroCols <- weightedRecom[,weightedRecom[11,]>0]
 
 
 df2 <- as.data.frame(cbind(countLevelOfSimilarity[,1],as.numeric(countLevelOfSimilarity[,2])))
 df2[,2]<- as.numeric(levels(df2[,2]))[as.integer(df2[,2])]
 df2[,1] <- as.character(df2[,1])
-df2[df2[1]=="-3685863687683693081",2] <- 0
+df2[df2[1]==targetUserID,2] <- 0
 sortedDF2 <- df2[order(df2[,2],decreasing=T),]
 similarUsers <- sortedDF2[1:10,1]
 distMatr <- matrix(nrow=11,ncol=1,data=0)
 colnames(distMatr) <- unique(weightsDF[,2])
 df33 <- data.frame()
-allPagesViewedBySelectedUsers <- sapply(c("-3685863687683693081",similarUsers),
+allPagesViewedBySelectedUsers <- sapply(c(targetUserID,similarUsers),
                                         function(x){
                                         df33 <<- rbind(df33,weightsDF[weightsDF[,1]==x,]);
                                         })
