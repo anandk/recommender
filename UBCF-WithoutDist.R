702261fa822ed6f800027b9b44a823cc14e20045
AK <anandk@thoughtworks.com>
2014-04-17 13:06:28 +0530

    Cleaned with comments

--- UBCF-WithoutDist.R
+++ UBCF-WithoutDist.R
@@ -1,25 +1,27 @@
 require("data.table")
 
-userColumn <- "user"
-itemColumn <- "itemIDs"
-weightColumn <- "weight"
-targetUserID <- "-3685863687683693081"
-
-
-setwd("C:/TW-Projects/PS-Projects/AbcamAnalytics/RSandbox/recommender/")
-userItemsWeightsDT <- fread(input="FinalWeightsCombined",sep="\t",colClasses=c("character","numeric","numeric"),header=FALSE)
-setnames(userItemsWeightsDT, c(userColumn,itemColumn,weightColumn))
-dtKeyedByItems <- copy(userItemsWeightsDT)
-setkey(userItemsWeightsDT,user)
-setkey(dtKeyedByItems, itemIDs)
-uniqueUsers <- unique(userItemsWeightsDT[,user])
-uniqueItems <- unique(dtKeyedByItems[,itemIDs])
-x1 <- function(targetUser){
-viewedPages <- userItemsWeightsDT[targetUser][,itemIDs]
-sortedRelevantUsers <- dtKeyedByItems[list(viewedPages)][, list(user,itemIDs)][,length(itemIDs),by=user][order(-V1),][2:11,]
-allItemsRelevantUsersHaveViewed <- userItemsWeightsDT[sortedRelevantUsers$user]
-recomm <- allItemsRelevantUsersHaveViewed[,sum(weight),by=itemIDs][order(-V1)][1:20,itemIDs]
-return (recomm)
+setup <- function(){
+  setwd("C:/TW-Projects/PS-Projects/AbcamAnalytics/RSandbox/recommender/")
+  userItemsWeightsDT <- fread(input="FinalWeightsCombined",sep="\t",colClasses=c("character","numeric","numeric"),header=FALSE)
+  setnames(userItemsWeightsDT, c("user","itemIDs","weight"))
+  dtKeyedByItems <- copy(userItemsWeightsDT)
+  setkey(userItemsWeightsDT,user)
+  setkey(dtKeyedByItems, itemIDs)
+  uniqueUsers <- unique(userItemsWeightsDT[,user])
+  uniqueItems <- unique(dtKeyedByItems[,itemIDs])
 }
 
-system.time(tmpTest <- sapply(uniqueUsers[1:10], function(x) {return (x1(x));}))
+getRecommendations <- function(targetUser){
+  viewedPages <- userItemsWeightsDT[targetUser][,itemIDs]
+  #Get the users who have viewed any page in the viewedPages list.
+  #Group by user & compute the number of pages in common with viewedPages
+  #Sort this in descending order & pick the top 10 excluding the target user (1st row)
+  sortedRelevantUsers <- dtKeyedByItems[list(viewedPages)][,length(itemIDs),by=user][order(-V1),][2:11,]
+  allItemsRelevantUsersHaveViewed <- userItemsWeightsDT[sortedRelevantUsers$user]
+  recomm <- allItemsRelevantUsersHaveViewed[,sum(weight),by=itemIDs][order(-V1)][1:20,itemIDs]
+  return (recomm)
+}
+
+system.time(setup())
+#Takes 16.3 sec for 1000 users on my system. Hence, for 100,000 we are looking at < 30 min.
+system.time(tmpTest <- t(sapply(uniqueUsers[1:10], function(x) {return (getRecommendations(x));})))
